3957df7a017d745dd14a83218c56fa3fd74455a9
Gavin <138510566+SHSFootballRepo@users.noreply.github.com>
2023-08-20 13:15:00 -0300

    Changes

--- .history/src/app/page_20230820121903.js
+++ .history/src/app/page_20230820121903.js
@@ -7,7 +7,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <HeaderComponent></HeaderComponent>
-        <p>Test</p>
         <FooterComponent></FooterComponent>
         </div>
     </main>
