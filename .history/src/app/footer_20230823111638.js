af8419589530d4a56136c90295167d3f13524176
SHSFootballRepo <ggr697385@gnspes.ca>
2023-08-23 12:15:27 -0300

    upgrades

--- .history/src/app/footer_20230823111638.js
+++
@@ -1,6 +0,0 @@
-function FooterComponent(){
-    return (
-        <div>I am Footer Component</div>
-    )
-}
-export default FooterComponent;
--- .history/src/app/header_20230820115652.js
+++
@@ -1,8 +0,0 @@
-function HeaderComponent() {
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
-}
-export default HeaderComponent;
